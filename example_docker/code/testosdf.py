e8102740b9690e9ae8b6ab92a821657f88767a85
Ricky Mok <cskpmok@caida.org>
2025-09-25 21:42:03 -0700

    update test code

--- example_docker/code/testosdf.py
+++ example_docker/code/testosdf.py
@@ -1,49 +1,45 @@
-import dpkt
 import socket
 import fsspec
 from pelicanfs.core import PelicanFileSystem,OSDFFileSystem
 import argparse
-from hashlib import md5
-from Crypto.Cipher import AES
 from fsspec.implementations.http import HTTPFileSystem
-
-#pelfs = PelicanFileSystem("pelican://osg-htc.org")
-#hello_world = pelfs.cat('/ospool/uc-shared/public/OSG-Staff/validation/test.txt')
-#print(hello_world)
-
-#with fsspec.open("osdf:///ospool/uc-shared/public/OSG-Staff/validation/test.txt", "rb") as open_test:
-#    line = open_test.readline()
-#    print(line)
+import logging
 
 
-# Open the pcap file in binary read mode
-#with pelfs.open("/home/cskpmok/ucsd-nt.1664589600.pcap", "rb") as f:
+logging.basicConfig()
+logging.getLogger().setLevel(logging.DEBUG)
+requests_log = logging.getLogger("requests.packages.urllib3")
+requests_log.setLevel(logging.DEBUG)
+requests_log.propagate = True
+requestsp_log = logging.getLogger("fsspec.pelican")
+requestsp_log.setLevel(logging.DEBUG)
+requestsp_log.propagate = True
+http_client_log = logging.getLogger("http.client")
+http_client_log.setLevel(logging.DEBUG)
+http_client_log.propagate = True
 
-
-
-def readpcap(input_path, passkey, token,output_path, endcnt=10):
+def readhelloworld(input_path, token):
+    #read the token file
     with open(token, "r") as f:
         token_str = f.read().strip()
+    #create Authorization Bearer HTTP header
     options = {"headers": {"Authorization": f"Bearer {token_str}"}}
-    #fsosdf = fsspec.filesystem("pelican", host="osg-htc.org", headers={"Authorization": f"Bearer {token_str}"})
-    
+
+    #configurate PelicanFS
     fsosdf = PelicanFileSystem("pelican://osg-htc.org", headers={"Authorization": f"Bearer {token_str}"})
 
-    #with fsspec.open(input_path, "rb") as f:
+    #read the hello world file from OSDF
     with fsosdf.open(input_path, "rb",) as f:
         print(f.readline())
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Read a pcap file and print packet info.")
-    parser.add_argument('-i', "--pcappath", help="Path to the pcap file")
-    parser.add_argument('-c',"--endcnt", type=int, default=10, help="Number of packets to process (default: 10)")
-    parser.add_argument('-o', "--output", help="Path to the output file")
-    parser.add_argument('-p', "--password", help="Password file for decryption if needed")
-    parser.add_argument('-t', "--token", help="Token for authentication if needed")
+    parser = argparse.ArgumentParser(description="Read the hello world file and print it out.")
+    parser.add_argument('-i', "--pcappath", help="Path to the hello world file")
+    parser.add_argument('-t', "--token", help="Token for authentication")
     args = parser.parse_args()
 
-    readpcap(args.pcappath, args.password, args.token,args.output, args.endcnt)
+    readhelloworld(args.pcappath, args.token)
 
 if __name__ == "__main__":
     main()
